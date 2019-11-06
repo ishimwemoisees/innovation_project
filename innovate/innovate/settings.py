2d6c3db80a01956c8cd89d2e3bb0a6626a7c42ea
nishimweaimee <nishimwemarieaimee1995@gmail.com>
2019-11-06 15:50:49 +0200

    update settings1

--- innovate/innovate/settings.py
+++ innovate/innovate/settings.py
@@ -137,5 +137,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
-prod_db = dj_database_url.config(conn_max_age=501)
+prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
